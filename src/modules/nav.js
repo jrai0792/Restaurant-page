4a592d60a4bd1bb2b7fe81b990ae410bb6fc4921
Jyoti <jyotikiran12@gmail.com>
2021-02-03 10:50:21 +0530

    Add style to navbar

--- src/modules/nav.js
+++ src/modules/nav.js
@@ -1,23 +1,42 @@
 function addNavBar() {
   const content = document.getElementById("content");
-  let div = document.createElement("div");
-  div.classList.add = "flex";
-  div.classList.add = "navbar";
-  let anchor = document.createElement("a");
-  anchor.href = "#";
-  anchor.innerText = "Home";
-  div.appendChild(anchor);
+  const container = document.createElement("div");
+  container.setAttribute('class','container');
+  const navbar = document.createElement("nav");
+  navbar.setAttribute('class','navbar');
+  const logo = document.createElement("h2");
+  logo.textContent = "Food Coma";
+  const ul = document.createElement("ul");
+  ul.setAttribute('class','navbar-nav, flex');
+  const liHome = document.createElement("li");
+  liHome.setAttribute('class','nav-item');
+  const aHome = document.createElement("a");
+  aHome.href = "#";
+  aHome.innerText = "Home";
+  aHome.classList.add("nav-link");
+  const liMenu = document.createElement("li");
+  liMenu.setAttribute('class','nav-item');
+  const aMenu = document.createElement("a");
+  aMenu.href = "#";
+  aMenu.innerText = "Menu";
+  aMenu.classList.add("nav-link");
+  const liInfo = document.createElement('li');
+  liInfo.setAttribute('class','nav-item');
+  const aInfo = document.createElement('a');
+  aInfo.href = "#";
+  aInfo.innerText = "Info";
+  aInfo.classList.add("nav-link");
+  liInfo.appendChild(aInfo);
+  liMenu.appendChild(aMenu);
+  liHome.appendChild(aHome);
+  ul.appendChild(liHome);
+  ul.appendChild(liMenu);
+  ul.appendChild(liInfo);
+  navbar.appendChild(logo);
+  navbar.appendChild(ul);
+  container.appendChild(navbar);
+  content.appendChild(container);
 
-  let anchor1 = document.createElement("a");
-  anchor1.href ="#";
-  anchor1.innerText = "Menu";
-  div.appendChild(anchor1);
-
-  let anchor2 = document.createElement("a");
-  anchor2.href = "#";
-  anchor2.innerText = "Contact";
-  div.appendChild(anchor2);
-  content.appendChild(div);
 }
 
 addNavBar();
