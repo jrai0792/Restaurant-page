00328a07705cf0442fa9fb17e165550984b5cd8b
Jyoti <jyotikiran12@gmail.com>
2021-02-05 21:21:41 +0530

    Fixed the start script issue

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
